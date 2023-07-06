4680d8339af54b2dbefea63eb07bd539c5e87341
Rahul Guha <69rahul16@gmail.com>
2023-07-06 19:11:40 +0530

    fixed config

---
+++ tx-history-from-alchemy-sdk.js
@@ -0,0 +1,38 @@
+import { Alchemy, Network } from "alchemy-sdk";
+import dotenv from "dotenv";
+
+const config = {
+  apiKey: process.env.Alchemy_api,
+  network: Network.MATIC_MAINNET,
+};
+const alchemy = new Alchemy(config);
+
+async function bigdata(_fromAddress) {
+  const data = await alchemy.core.getAssetTransfers({
+    fromBlock: "0x0",
+    fromAddress: _fromAddress,
+    category: ["internal"],
+  });
+  let sum = 0;
+  data.transfers.forEach((Element) => {
+    sum += Element.value;
+  });
+  //console.log(data.transfers[0].value);
+  return sum;
+}
+
+async function totalM() {
+  const eggsPromise = bigdata("0x534bD4C7aa15116Fb7917e74591622Ba55AcB8d0");
+  const runesPromise = bigdata("0xe51f9dD4681F1bdB7fe2C52e5193457c784f25d6");
+  const rafflePromise = bigdata("0x03268806E04d0a0149d8F1CCe2762fd0be95e3aa");
+  const [eggs, runes, raffle] = await Promise.all([
+    eggsPromise,
+    runesPromise,
+    rafflePromise,
+  ]);
+
+  const total = eggs + runes + raffle;
+  console.log(`Total matic printed is ${total}`);
+}
+
+totalM();
