98540b70a05abf3146e1d51326351ec29bb3f839
Rahul Guha <69rahul16@gmail.com>
2023-07-06 20:01:18 +0530

    fixed

--- tx-history-from-alchemy-sdk.js
+++ tx-history-from-alchemy-sdk.js
@@ -2,7 +2,7 @@ import { Alchemy, Network } from "alchemy-sdk";
 // import dotenv from "dotenv";
 
 const config = {
-  apiKey: process.env.alchemy_api,
+  apiKey: process.env.Alchemy_api,
   network: Network.MATIC_MAINNET,
 };
 const alchemy = new Alchemy(config);
@@ -22,8 +22,8 @@ async function bigdata(_fromAddress) {
 }
 
 async function totalM() {
-  const eggsPromise = bigdata("0x534bD4C7aa15116Fb7917e74591622Ba55AcB8d0");
-  const runesPromise = bigdata("0xe51f9dD4681F1bdB7fe2C52e5193457c784f25d6");
+  const eggsPromise = bigdata("0xe51f9dD4681F1bdB7fe2C52e5193457c784f25d6");
+  const runesPromise = bigdata("0x534bD4C7aa15116Fb7917e74591622Ba55AcB8d0");
   const rafflePromise = bigdata("0x03268806E04d0a0149d8F1CCe2762fd0be95e3aa");
   const [eggs, runes, raffle] = await Promise.all([
     eggsPromise,
@@ -31,7 +31,7 @@ async function totalM() {
     rafflePromise,
   ]);
 
-  const total = eggs + runes + raffle;
+  const total = raffle + eggs + runes;
   console.log(`Total matic printed is ${total}`);
 }
 
